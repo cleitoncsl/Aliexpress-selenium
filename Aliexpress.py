0ffade997ea93c91ffc6390b275f6eb12d5fdfdf
Cleiton da Silva Leal <cleiton_csl@live.com>
2022-02-05 17:35:17 -0300

    versão 2.1.5 tentando criar um json

--- Aliexpress.py
+++ Aliexpress.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 from collections import Counter
 
-
 url = "https://pt.aliexpress.com/category/201054516/tablets.html?spm=a2g0o.category_nav.1.369.3770609dA0wBh7"
 
 sleep(1) # Time in seconds
@@ -89,6 +88,16 @@ lista_produtos = soup.find_all('a', attrs={'class': '_3t7zg _2f4Ho'})
 sleep(1)
 
 contador = 0
+set_produto = []
+set_preco_produto = []
+set_qtde_vendas = []
+set_tipo_frete = []
+set_tipo_devolucao = []
+set_avaliacoes = []
+set_loja = []
+set_link_produto = []
+set_promocao = []
+set_top_selling = []
 
 for produto in lista_produtos:
     sleep(0.1)
@@ -96,63 +105,110 @@ for produto in lista_produtos:
     nome_produto = produto.select('h1._18_85')
     sleep(0.1)
     print(f'Nome -> {nome_produto[0].getText()}')
+
+    vNome_produto = nome_produto[0].text
     preco_produto = produto.find('div', class_="mGXnE _37W_B").select('a span')
-    sleep(0.1)
-    try:
-        print(f'Preço -> {preco_produto[0].getText() + preco_produto[1].getText() + preco_produto[2].getText() + preco_produto[3].getText()}')
-    except Exception as e:
-        print(f'Preço -> {preco_produto[0].getText() + preco_produto[1].getText()}')
+    set_produto.append(vNome_produto)
 
     sleep(0.1)
+
+    try:
+        print(f'Preço -> {preco_produto[0].getText() + preco_produto[1].getText() + preco_produto[2].getText() + preco_produto[3].getText()}')
+        vPreco_produto = {
+            preco_produto[0].getText() + preco_produto[1].getText() + preco_produto[2].getText() + preco_produto[3].getText()}
+
+    except Exception as e:
+        print(f'Preço -> {preco_produto[0].getText() + preco_produto[1].getText()}')
+        vPreco_produto = {
+            preco_produto[0].getText() + preco_produto[1].getText()}
+
+    set_preco_produto.append(vPreco_produto)
+
+    sleep(0.1)
+
     qtde_vendas = produto.select("span._1kNf9")
     sleep(0.1)
     try:
         if qtde_vendas is None:
             print('qtde_vendas -> -')
+            vQtde_Vendas = qtde_vendas[0].getText()
+
         else:
             print(f'qtde_vendas -> {qtde_vendas[0].getText()}')
+            vQtde_Vendas = qtde_vendas[0].getText()
+
     except Exception as e:
         print(f'qtde_vendas -> -')
+        vQtde_Vendas
+
+    set_qtde_vendas.append(vQtde_Vendas)
 
     tipo_frete = produto.select("span._2jcMA")
     sleep(0.1)
     try:
-        if tipo_frete is None:
+        if len(tipo_frete) == 0:
             print('Frete -> -')
+            vTipo_Frete
+
         else:
             print(f'Frete -> {tipo_frete[0].getText()}')
+            vTipo_Frete = tipo_frete[0].getText()
+
     except Exception as e:
         print(f'Frete -> -')
+        vTipo_Frete = tipo_frete[0].getText()
+
+    set_tipo_frete.append(vTipo_Frete)
 
     tipo_devolucao = produto.select("span._2jcMA")
     sleep(0.1)
     try:
         if tipo_devolucao is None:
             print('Frete -> -')
+            vTipo_Devolucao = tipo_devolucao[0].getText()
+
         else:
             print(f'Devolução -> {tipo_devolucao[1].getText()}')
+            vTipo_Devolucao = tipo_devolucao[0].getText()
+
     except Exception as e:
         print(f'Devolução -> -')
+        vTipo_Devolucao = "-"
+
+    set_tipo_devolucao.append(vTipo_Devolucao)
 
     avaliacoes = produto.select("span.eXPaM")
     sleep(0.1)
     try:
         if avaliacoes is None:
             print('Avaliações -> -')
+            vAvaliacoes = "-"
         else:
             print(f'Avaliações -> {avaliacoes[0].getText()}')
+            vAvaliacoes = avaliacoes[0].getText()
+
     except Exception as e:
         print(f'Avaliações -> -')
+        vAvaliacoes = "-"
+
+    set_avaliacoes.append(vAvaliacoes)
 
     loja = produto.select("span._7CHGi")
     sleep(0.1)
     try:
         if loja is None:
             print('Loja -> -')
+            vLoja = "-"
+
         else:
             print(f'Loja -> {loja[0].getText()}')
+            vLoja = loja[0].getText()
+
     except Exception as e:
         print(f'Loja -> -')
+        vLoja = "-"
+
+    set_loja.append(vLoja)
 
     link_produto = [a['href'] for a in soup.find_all('a', href=True) if a.text]
     if contador == 0:
@@ -167,37 +223,66 @@ for produto in lista_produtos:
     try:
         if link_produto_final is None:
             print('Loja -> -')
+            vLinkProdutoFinal = "-"
+
         else:
             print(f'Loja -> {site + link_produto_final}')
+            vLinkProdutoFinal = site + link_produto_final
 
     except Exception as e:
         print(f'Loja -> -')
+        vLinkProdutoFinal = "-"
+
+    set_link_produto.append(vLinkProdutoFinal)
 
     promocao = produto.find_all('img', class_='_1mroo')
     len_promocao = len(promocao)
     texto_promocao = str(promocao)
+
     sleep(0.1)
+
     try:
         if texto_promocao.count('https') is None:
             print('Promocao? -> (n)')
+            vPromocao = "(n)"
+
         else:
             print(f'Promocao? -> (s)')
+            vPromocao = "(s)"
+
     except Exception as e:
         print(f'Loja -> -')
+        vPromocao = "(n)"
 
-    top_selling  = produto.find_all('img', class_='_1mroo')
+    set_promocao.append(vPromocao)
+
+    top_selling = produto.find_all('img', class_='_1mroo')
     len_top_selling  = len(top_selling )
     texto_top_selling  = str(top_selling )
+
     sleep(0.1)
+
     try:
         if top_selling.count('https') is None:
-            print('Promocao? -> (n)')
+            print('top_selling? -> (n)')
+            v_top_selling = "(n)"
+
         else:
-            print(f'Promocao? -> (s)')
+            if len_top_selling == 2:
+                print(f'top_selling? -> (s)')
+                v_top_selling = "(s)"
+
+            else:
+                print(f'top_selling? -> (n)')
+                v_top_selling = "(n)"
+
     except Exception as e:
         print(f'Loja -> -')
+        v_top_selling = "(n)"
+
+
+    set_top_selling.append(v_top_selling)
 
     contador += 1
-
 
 navegador.quit()
